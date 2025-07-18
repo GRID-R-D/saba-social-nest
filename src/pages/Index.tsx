cbd980c96d3f631a0250702fc9deca9c3da01907
tejassp03 <97590513+tejassp03@users.noreply.github.com>
2025-07-18 10:27:11 +0530

    Removed footer button

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -655,9 +655,6 @@ const Index = () => {
               <Calendar className="w-5 h-5 mr-2" />
               Book Your Session Now
             </Button>
-            <Button variant="outline" size="xl" className="border-white text-white hover:bg-white hover:text-primary-dark">
-              Explore Our Plans
-            </Button>
           </div>
         </div>
       </section>
